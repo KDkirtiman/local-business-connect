6352dfc218cb535136bd1d43ba1380fd90363d06
KDkirtiman <kirtimansingh205@gmail.com>
2024-01-22 22:14:52 +0530

    7-develop-card-component : Added TextField and TextArea component from material-ui

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import LBCCard from './Components/Card/Card';
 import LBCLabel from './Components/Label/Label';
+import LBCTextField from './Components/TextField/TextField';
+import TextFieldDiv from './StyledComponent/StyledComponent';
 
 function App() {
   return (
@@ -15,6 +17,12 @@ function App() {
           <LBCLabel variant={"h3"}>
             This is Label Component.
           </LBCLabel>
+          <TextFieldDiv>
+            <LBCTextField label="Standard" variant="standard"/>
+          </TextFieldDiv>
+          <TextFieldDiv>
+            <LBCTextField label="Multiline" variant="outlined"/>
+          </TextFieldDiv>
         </LBCCard>
       </header>
     </div>
