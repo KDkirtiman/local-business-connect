cd480ced4ceb5ca0c83949c03a3cf60256415c33
KDkirtiman <kirtimansingh205@gmail.com>
2024-02-02 22:44:07 +0530

    15-create-radio-button-component : Added Basic structure of Radiobutton

---
+++ src/Components/Radiobutton/Radiobutton.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+function LBCRadioButton(props) {}
+
+export default LBCRadioButton;
---
+++ src/Components/Radiobutton/RadiobuttonLabel.styled.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+const RadioButtonLabel = styled.label``;
+
+export default RadioButtonLabel;
