5b0d794f7dc385afbc43ecafa18b2e996a681acf
KDkirtiman <kirtimansingh205@gmail.com>
2024-02-02 23:09:29 +0530

    15-create-radio-button-component : Added oncheck functionality of Radiobutton

--- src/Components/Radiobutton/Radiobutton.jsx
+++ src/Components/Radiobutton/Radiobutton.jsx
@@ -1,5 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import RadioButton from './Radiobutton.styled';
+import RadioButtonLabel from './RadiobuttonLabel.styled';
 
-function LBCRadioButton(props) {}
+function LBCRadioButton(props) {
+  const [optionsList, setOptionsList] = useState(props.optionsList);
+
+  const handleRadioButtonCheck = (event) => {
+    const selectedValue = event.target.value;
+    const newOptionsList = optionsList.map((item) => {
+      if(item.value === selectedValue) {
+        return {...item, checked: true}
+      } else {
+        return item;
+      }
+    });
+    setOptionsList(newOptionsList);
+  }
+  return (<>
+    {
+      (props.optionsList && props.optionsList.length > 0) &&
+        props.optionsList.map((item)=>{
+          return (
+            <>
+              <RadioButton
+                type='radio'
+                value={item.value}
+                name={item.name}
+                checked={item.checked}
+                onClick={handleRadioButtonCheck}
+              />
+              <RadioButtonLabel/>
+            </>
+          )
+        })
+    }
+  </>); 
+}
 
 export default LBCRadioButton;
