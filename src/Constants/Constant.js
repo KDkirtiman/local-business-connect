7cf52b8b3ee38011b61cd2ef9ac5c0a20accc7c9
KDkirtiman <kirtimansingh205@gmail.com>
2024-02-14 20:12:09 +0530

    22-create-login-module : Added Authentication and Error Overlay for the application

---
+++ src/Constants/Constant.js
@@ -0,0 +1,6 @@
+const LBCCONSTANT = {
+    authenticateFailure: "Username/Password Incorrect",
+    authenticateNoUserFound: "User Not Found"
+}
+
+export default LBCCONSTANT;
--- src/Module/Login/Login.jsx
+++ src/Module/Login/Login.jsx
@@ -5,10 +5,16 @@ import LBCLabel from "../../Components/Label/Label";
 import LBCTextField from "../../Components/TextField/TextField";
 import { AuthBtnWrapper, AuthenticateButton, PasswordDiv, UserNameDiv } from "./Login.styled";
 import LBCButton from "../../Components/Button/Button";
+import authenticateUser from "../../ApiRequest/AuthenticateUser/AuthenticateUser";
+import { useNavigate } from "react-router-dom";
+import { LBCErrorBox } from "../../Components/ErrorBox/ErrorBox";
+import LBCCONSTANT from "../../Constants/Constant";
 
 function LoginOverlay (props) {
+    const navigate = useNavigate();
     const [userName, setUserName] = useState('');
     const [password, setPassword] = useState('');
+    const [displayErrorPopup, setDisplayErrorPopup] = useState(false);
 
     const onChangeUserName = (event) => {
         setUserName(event.target.value);
@@ -18,8 +24,18 @@ function LoginOverlay (props) {
         setPassword(event.target.value);
     }
 
-    const handleLogin = () => {
-        console.log("User : %s is logged in",userName);
+    const handleLogin = async () => {
+        const response = await authenticateUser();
+        console.log("User : {} is logged in",response);
+        if (!response || !response.transactionSuccess || response.error) {
+            setDisplayErrorPopup(true);
+        } else {
+            navigate("/practice");
+        }
+    }
+
+    const  closeErrorPopUp = () => {
+        setDisplayErrorPopup(false);
     }
 
     return (
@@ -49,6 +65,7 @@ function LoginOverlay (props) {
                 <AuthBtnWrapper>
                     <AuthenticateButton label={"Authenticate"} onClick={handleLogin}/>
                 </AuthBtnWrapper>
+                { displayErrorPopup && <LBCErrorBox errorMsg={LBCCONSTANT.authenticateFailure} onClose={closeErrorPopUp} /> }
                 <div>Forget Password</div>
                 <div>Are you New User?</div>
             </LBCOverlay>
